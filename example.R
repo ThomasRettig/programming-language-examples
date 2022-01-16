7316d43978e2b528867ea8c3417deb95faccbb4b
Thomas Rettig <thomas.rettig.11@gmail.com>
2022-01-16 21:18:39 +0800

    Initial commit

---
+++ example.R
@@ -0,0 +1,13 @@
+readinteger <- function()
+{ 
+  n <- readline(prompt="Enter an integer: ")
+  if(!grepl("^[0-9]+$",n))
+  {
+    return(readinteger())
+  }
+  
+  return(as.integer(n))
+}
+
+print(readinteger())
+ 
